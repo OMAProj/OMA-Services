5eee6396c1f0738ed3cc5f6699cd9e62bf51d5a5
Praveen-751 <chinnamsettipraveenkumar@gmail.com>
2023-11-13 23:36:59 +0530

    Authtoken

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,38 @@
 // routes/auth.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
 
-router.post('/login', (req, res) => {
+const JWT_SECRET = "eCp3sR6u9xw2z$B&E(H+MbQeThVmYq3t";
+
+const users = [
+  {
+    id: 1,
+    email: "praveen@gmail.com",
+    password: "12345678",
+  },
+  {
+    id: 2,
+    email: "omaadmin@gmail.com",
+    password: "Admin@123",
+  },
+];
+
+router.post("/login", (req, res) => {
   // Implement your login logic here
-  console.log("called",req);
   const { email, password } = req.body;
 
   // Dummy logic (replace with actual authentication logic)
-  if (email === 'praveen@gmail.com' && password === '12345678') {
-    res.json({ success: true, message: 'Login successful' });
+  const user = users.find((u) => u.email === email && u.password === password);
+
+  if (user) {
+    // Generate JWT token
+    const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: "1h" });
+
+    // Send the token as a response
+    res.json({ authtoken: token, message: "Loggedin Succesfully" });
   } else {
-    res.status(401).json({ success: false, message: 'Invalid credentials' });
+    res.status(401).json({ message: "Invalid credentials" });
   }
 });
 
